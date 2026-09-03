d4a8f25c8dea9869e56c109e831154b6b2164cd9
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-03 19:07:46 +0800

    Day02.java

--- Day02.java
+++ Day02.java
@@ -3,7 +3,7 @@ class Day02 {
         System.out.println("=== PERKENALKAN DIRI ===");
 
         System.out.println("Nama       : Rahmat");
-        System.out.println("Nim       : "H0726304);
+        System.out.println("Nim        : H0726304");
         System.out.println("Prodi      : Pendidikan Teknologi Informasi");
         System.out.println("Tempat     : Campalagian");
         System.out.println("Hobi       : Membuat Video");
