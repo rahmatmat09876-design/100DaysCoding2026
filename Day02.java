216e0aa7d308a184871433f804922e34df40e978
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-03 20:26:39 +0800

    Day02.java

--- Day02.java
+++ Day02.java
@@ -1,13 +1,18 @@
 class Day02 {
     public static void main(String[] args) {
-        System.out.println("=== PERKENALKAN DIRI ===");
+        //MENGGUNAKAN println
+        System.out.println("PERKENALKAN DIRI");
 
-        System.out.println("Nama       : Rahmat");
-        System.out.println("Nim        : H0726304");
-        System.out.println("Prodi      : Pendidikan Teknologi Informasi");
-        System.out.println("Tempat     : Campalagian");
-        System.out.println("Hobi       : Membuat Video");
+        //MENGGUNAKAN print() + println()
+        System.out.print("Nama:");
+        System.out.println("Rahmat");
+        
+        System.out.print("NIM:");
+        System.out.println("H0726304");
 
-        //DONE YAA KAKAK KAKAKK
+        //MENGGUNAKAN printf()
+        System.out.printf("Prodi  : %s%n", "Pendidikan Teknologi Informasi");
+        System.out.printf("Tempat : %s%n", "Campalagian");
+        System.out.printf("Hobi   : %s%n", "Membuat Video");
     }
 }
