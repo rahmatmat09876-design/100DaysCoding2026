23e1d394a862d8be715457758cb59ea871c8e34b
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-04 22:05:44 +0800

    Day03.java

---
+++ Day03.java
@@ -0,0 +1,16 @@
+public class Day03 {
+    public static void main(String[] args) {
+
+        System.out.println("Nama:Rahmat\nUmur: 19 tahun");
+        // Membuat teks berikutnya tampil di baris baru
+
+        System.out.println("Alamat:\tCampalagian");
+        // Memberikan spasi menggunakan tab
+
+        System.out.println("PTI: \"Pendidikan Teknologi Informasi!\"");
+        // Menampilkan karakter tanda kutip
+
+        System.out.println("Link: https://contoh.com\\belajar");
+        // Menampilkan simbol backslash
+    }
+}
