6a7a1dd22bd20485f97e1666c7003a516d55dc5a
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-05 22:09:31 +0800

    Day04.java

---
+++ Day04.java
@@ -0,0 +1,23 @@
+public class 04 {
+    public static void main(String[] args) {
+
+      //semangat matemann
+        byte umur = 19;
+        // byte untuk menyimpan angka yang kecil
+
+        short jumlahBarang = 2500;
+        // short digunakan untuk angka yang lebih besar dari byte
+
+        int hargaBarang = 750000;
+        // int sering digunakan untuk menyimpan bilangan bulat
+
+        long jumlahPenduduk = 280000000L;
+        // long digunakan untuk menyimpan angka yang sangat besar
+        // huruf L menunjukkan bahwa nilai tersebut bertipe long
+
+        System.out.println("Umur          : " + umur);
+        System.out.println("Jumlah barang : " + jumlahBarang);
+        System.out.println("Harga barang  : " + hargaBarang);
+        System.out.println("Penduduk      : " + jumlahPenduduk);
+    }
+}
