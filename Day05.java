7534b27be1fc0ccf317b79f2f07befc0afe6864a
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-06 21:41:26 +0800

    Day05.java

---
+++ Day05.java
@@ -0,0 +1,13 @@
+public class Day05 {
+    public static void main(String[] args) {
+
+        double tinggiBadan = 160.2;
+        // double untuk menyimpan angka pecahan
+
+        float beratBadan = 55.5f;
+        // float untuk menyimpan angka pecahan
+        System.out.println("SEPUTAR TINGGI BADAN SAYA DAN BB SAYA");
+        System.out.println("Tinggi badan : " + tinggiBadan + " cm");
+        System.out.println("Berat badan  : " + beratBadan + " kg");
+    }
+}
