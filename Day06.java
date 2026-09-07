4f5cf4c52f5c99ccb07b98c7dd8b2b37d1230647
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-07 22:40:20 +0800

    Day06.java

---
+++ Day06.java
@@ -0,0 +1,19 @@
+public class Day06 {
+    public static void main(String[] args) {
+        
+        //Tipe Data Karakter (char)
+        char jenisKelamin = 'L';
+        char nilai = 'A';
+        char simbol = '$';
+
+        //Tipe Data Boolean (boolean)
+        boolean isLulus = true;
+        boolean isHujan = false;
+
+        // Menampilkan nilai variabel ke konsol
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Apakah Lulus? " + isLulus);
+        System.out.println("Apakah Hujan? " + isHujan);
+    }
+}
