ebdf84fc2f4b464b0fe330023739294801ae3014
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-08 21:57:58 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,10 @@
+public class Day07 {
+    public static void main(String[] args) {
+
+        // String digunakan untuk menyimpan data berupa teks atau kumpulan karakter.
+        String nama = "Rahmat";
+
+        // Menampilkan teks yang tersimpan dalam variabel String.
+        System.out.println("Nama saya: " + nama);
+    }
+}
