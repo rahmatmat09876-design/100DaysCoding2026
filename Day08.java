75337b9f0992156fb5053bee600137e43cb9250b
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-09 21:14:11 +0800

    Day08.java

---
+++ Day08.java
@@ -0,0 +1,24 @@
+public class Day08 {
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        String mahasiswa;
+        int umur;
+
+        // Inisialisasi variabel
+        mahasiswa = "Rahmat";
+        umur = 18;
+
+        System.out.println("Data Awal:");
+        System.out.println("Nama : " + mahasiswa);
+        System.out.println("Umur : " + umur + " tahun");
+
+        // Update variabel
+        mahasiswa = "Rahmat PTI";
+        umur = 19;
+
+        System.out.println("\nSetelah diubah:");
+        System.out.println("Nama : " + mahasiswa);
+        System.out.println("Umur : " + umur + " tahun");
+    }
+}
