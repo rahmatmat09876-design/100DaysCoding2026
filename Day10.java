2d7f4066478ac20715fcf323448b7bc9aae304fa
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-11 22:39:57 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,13 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // Tipe Data Bulat
+        System.out.println("byte    : " + Byte.MIN_VALUE + " s/d " + Byte.MAX_VALUE);
+        System.out.println("short   : " + Short.MIN_VALUE + " s/d " + Short.MAX_VALUE);
+        System.out.println("int     : " + Integer.MIN_VALUE + " s/d " + Integer.MAX_VALUE);
+        System.out.println("long    : " + Long.MIN_VALUE + " s/d " + Long.MAX_VALUE);
+        
+        // Tipe Data Desimal
+        System.out.println("float   : " + Float.MIN_VALUE + " s/d " + Float.MAX_VALUE);
+        System.out.println("double  : " + Double.MIN_VALUE + " s/d " + Double.MAX_VALUE);
+    }
+}
