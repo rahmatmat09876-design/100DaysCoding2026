cf92d50b155e7b43aa10936b26fc7eb3dda3ceb6
rahmatmat09876-design <rahmatmat09876@gmail.com>
2026-09-15 21:57:19 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,13 @@
+public class Day14 {
+    public static void main(String[] args) {
+
+        // Operator Aritmatika Penjumlahan (+) dan Pengurangan (-)
+        int nilai1 = 75;
+        int nilai2 = 25;
+        int nilai3 = 10;
+
+        System.out.println("Hasil penjumlahan : " + (nilai1 + nilai2));
+        System.out.println("Hasil pengurangan : " + (nilai1 - nilai2));
+        System.out.println("Hasil operasi gabungan : " + (nilai1 + nilai2 - nilai3));
+    }
+}
